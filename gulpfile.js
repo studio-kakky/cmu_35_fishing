2c87e1d0eb82e422a8a0fb5828141f41cc047652
柿内暢昌 <nob@studio-kakky.com>
2015-12-18 17:10:45 +0900

    第1フェーズ

--- gulpfile.js
+++ gulpfile.js
@@ -16,18 +16,18 @@ gulp.task('copy:reveal', function() {
   .pipe(gulp.dest(DESTDIR+'lib/reveal'));
 });
 
-gulp.task('copy:html', function() {
-  return gulp.src('presentation/src/**/*.*')
+gulp.task('copy:html',['copy:reveal'],function() {
+  return gulp.src('presentation/src/**/*.html')
     .pipe(gulp.dest(DESTDIR));
 });
 
-gulp.task('copy:img', function() {
+gulp.task('copy:img',['copy:html'], function() {
   return gulp.src('presentation/img/**/*.*')
     .pipe(gulp.dest(DESTDIR+'img'));
 });
 
 
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', ['copy:img'], function() {
     browserSync({
         server: {
             baseDir: 'dest/'
@@ -44,8 +44,8 @@ gulp.task('browser-sync:demo', function() {
 });
 
 gulp.task('watch', function() {
-  gulp.watch('presentation/src/**/*.*',['copy:html']);
+  gulp.watch('presentation/src/**/*.*',['copy:img']);
 });
 
 
-gulp.task('default',['copy:html','copy:img','browser-sync','watch']);
+gulp.task('default',['browser-sync','watch']);
